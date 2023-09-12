1c5cb87c7a2fa2b1eed82fb74136274a7d430768
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-09-12 14:18:00 +0200

    fix: delete files

--- rekognitionlambdaTs/replaceSubstringWithColon.ts
+++
@@ -1,2 +0,0 @@
-export const replaceSubstringWithColon = (txt: string): string =>
-  txt.replace("%3A", ":");
--- rekognitionlambdaTs/streamToBuffer.ts
+++
@@ -1,9 +0,0 @@
-import { Readable } from "stream";
-
-export const streamToBuffer = async (stream: Readable): Promise<Buffer> =>
-  new Promise<Buffer>((resolve, reject) => {
-    const chunks: Buffer[] = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.once("end", () => resolve(Buffer.concat(chunks)));
-    stream.once("error", reject);
-  });
