5cf20a756201ee939f110838cccf62adfd46af9b
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-10-13 14:58:49 +0200

    fix: refactor & use queryCommand in stead of scanItems

--- rekognitionlambda/index.spec.ts
+++ rekognitionlambda/index.spec.ts
@@ -1,22 +1,15 @@
 import { describe, test, before } from "node:test";
 import assert from "node:assert";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import {
-  DeleteObjectCommand,
-  PutObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import path from "path";
 import { readFile } from "fs/promises";
-import {
-  DeleteCommand,
-  DynamoDBDocumentClient,
-  ScanCommand,
-} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { CloudFormationClient } from "@aws-sdk/client-cloudformation";
 import pRetry from "p-retry";
 import { stackOutput } from "@nordicsemiconductor/cloudformation-helpers";
 import type { StackOutputs } from "../lib/aws_dev_hour-stack";
+import { randomUUID } from "node:crypto";
 
 const db = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(db);
@@ -24,7 +17,7 @@ const s3 = new S3Client({});
 const CFclient = new CloudFormationClient();
 
 const image = path.join(process.cwd(), "./rekognitionlambda/cats.jpeg");
-const key = `cats.jpeg`;
+const key = randomUUID() + ".jpeg";
 
 let thumbBucketName: string;
 let tableName: string;
@@ -39,33 +32,10 @@ describe("e2e-tests", () => {
     bucketName = outputs.imageBucket;
     thumbBucketName = outputs.resizedBucket;
     tableName = outputs.ddbTable;
-    //delete images in both s3 buckets before running test
-    await s3.send(
-      new DeleteObjectCommand({
-        Bucket: bucketName,
-        Key: key,
-      })
-    );
-    console.log("deleted image in original bucket");
-    await s3.send(
-      new DeleteObjectCommand({
-        Bucket: thumbBucketName,
-        Key: key,
-      })
-    );
-    console.log("deleted image in thumbBucket");
-    await docClient.send(
-      new DeleteCommand({
-        TableName: tableName,
-        Key: { image: key },
-      })
-    );
-    console.log("deleted labels in dynamoDB");
   });
-  test("uploading an image to the bucket should trigger the handler", async () => {
-    //uploading an image to the bucket should trigger the handler
+  test("uploading an image to the bucket should trigger the handler and upload labels to DynamoDB", async () => {
     const uploadImage = await readFile(image);
-
+    //Putting the image in the s3 bucket
     await s3.send(
       new PutObjectCommand({
         Bucket: bucketName,
@@ -73,20 +43,7 @@ describe("e2e-tests", () => {
         Body: uploadImage,
       })
     );
-  });
-  const getTableItems = async (tableName: string) => {
-    const res = await docClient.send(
-      new ScanCommand({
-        TableName: tableName,
-      })
-    );
-    if (res.Count === 0) {
-      throw new Error("No items in table");
-    }
-    return res;
-  };
 
-  test("labels should be put in dynamodb", async () => {
     //Check if the labels are in DynamoBD, if not try again up to 5 times.
     const res = await pRetry(() => getTableItems(tableName), {
       onFailedAttempt: (error) => {
@@ -94,11 +51,29 @@ describe("e2e-tests", () => {
       },
       retries: 5,
     });
-    //Check that there exists an item in DynamoDB
-    assert.equal(res.Count, 1);
-    //Check that the item has the correct key
-    if (res.Items !== undefined) {
-      assert.equal(res.Items[0].image, key);
+
+    if (res.Items === undefined) {
+      assert.fail("Item in DynamoDB is undefined");
     }
+    //Check that the item has the correct labels
+    assert.equal(res.Items[0].Object1.S, "Animal");
+    assert.equal(res.Items[0].Object2.S, "Cat");
+    assert.equal(res.Items[0].Object3.S, "Mammal");
   });
 });
+
+const getTableItems = async (tableName: string) => {
+  const res = await docClient.send(
+    new QueryCommand({
+      TableName: tableName,
+      KeyConditionExpression: "image = :key",
+      ExpressionAttributeValues: {
+        ":key": { S: key },
+      },
+    })
+  );
+  if (res.Count === 0) {
+    throw new Error("No items in table");
+  }
+  return res;
+};
