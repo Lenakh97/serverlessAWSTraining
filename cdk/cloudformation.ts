60e3876d158c425cdc7a412e1cf4b42b9604f0f5
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-10-30 17:10:36 +0100

    fix: execute CDK app
    When converting this to TypeScript we cannot rely on
    Node.js executing the file, but we need to explicitely
    call the app.

---
+++ cdk/cloudformation.ts
@@ -0,0 +1,49 @@
+import { AwsDevHourStack } from "../lib/aws_dev_hour-stack.js";
+import { App } from "aws-cdk-lib";
+import pJSON from "../package.json";
+import { ensureGitHubOIDCProvider } from "../lib/ensureGitHubOIDCProvider.js";
+import { IAMClient } from "@aws-sdk/client-iam";
+
+const repoUrl = new URL(pJSON.repository.url);
+const repository = {
+  owner: repoUrl.pathname.split("/")[1] ?? "bifravst",
+  repo: repoUrl.pathname.split("/")[2]?.replace(/\.git$/, "") ?? "rest-echo",
+};
+
+const iam = new IAMClient({});
+
+export type Repository = {
+  owner: string;
+  repo: string;
+};
+
+export class AwsDevHourApp extends App {
+  public constructor({
+    repository,
+    gitHubOIDCProviderArn,
+    version,
+  }: {
+    repository: Repository;
+    gitHubOIDCProviderArn: string;
+    version: string;
+  }) {
+    super({
+      context: {
+        version,
+      },
+    });
+
+    new AwsDevHourStack(this, {
+      repository,
+      gitHubOIDCProviderArn,
+    });
+  }
+}
+
+new AwsDevHourApp({
+  repository,
+  gitHubOIDCProviderArn: await ensureGitHubOIDCProvider({
+    iam,
+  }),
+  version: process.env.VERSION ?? "0.0.0-development",
+});
