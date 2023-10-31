fc69a0101cc10504546972222258f304a7c985ef
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-10-31 14:35:18 +0100

    feat: implement continuous deployment with OIDC

---
+++ lib/CD.ts
@@ -0,0 +1,40 @@
+import { Duration, aws_iam as IAM, Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export type Repository = {
+  owner: string;
+  repo: string;
+};
+export class CD extends Construct {
+  public readonly role: IAM.IRole;
+  constructor(
+    parent: Stack,
+    {
+      repository: r,
+      gitHubOIDC,
+    }: {
+      repository: Repository;
+      gitHubOIDC: IAM.IOpenIdConnectProvider;
+    }
+  ) {
+    super(parent, "cd");
+
+    this.role = new IAM.Role(this, "ghRole", {
+      roleName: `${parent.stackName}-github-actions`,
+      assumedBy: new IAM.WebIdentityPrincipal(
+        gitHubOIDC.openIdConnectProviderArn,
+        {
+          StringEquals: {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub": `repo:${r.owner}/${r.repo}:environment:production`,
+          },
+        }
+      ),
+      description: `This role is used by GitHub Actions for CI of ${r.owner}/${r.repo}`,
+      maxSessionDuration: Duration.hours(1),
+      managedPolicies: [
+        IAM.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess"),
+      ],
+    });
+  }
+}
---
+++ lib/ensureGitHubOIDCProvider.ts
@@ -0,0 +1,58 @@
+import {
+  CreateOpenIDConnectProviderCommand,
+  GetOpenIDConnectProviderCommand,
+  IAMClient,
+  ListOpenIDConnectProvidersCommand,
+} from "@aws-sdk/client-iam";
+import chalk from "chalk";
+
+/**
+ * Returns the ARN of the OpenID Connect provider for GitHub of the account.
+ */
+export const ensureGitHubOIDCProvider = async ({
+  iam,
+}: {
+  iam: IAMClient;
+}): Promise<string> => {
+  const { OpenIDConnectProviderList } = await iam.send(
+    new ListOpenIDConnectProvidersCommand({})
+  );
+  const maybeGithubProvider = (
+    await Promise.all(
+      OpenIDConnectProviderList?.map(async ({ Arn }) =>
+        iam
+          .send(
+            new GetOpenIDConnectProviderCommand({
+              OpenIDConnectProviderArn: Arn,
+            })
+          )
+          .then((provider) => ({ Arn, provider }))
+      ) ?? []
+    )
+  ).find(
+    ({ provider: { Url } }) => Url === "token.actions.githubusercontent.com"
+  );
+  if (maybeGithubProvider?.Arn !== undefined) {
+    console.debug(
+      chalk.green(`OIDC provider for GitHub exists: ${maybeGithubProvider.Arn}`)
+    );
+    return maybeGithubProvider.Arn;
+  }
+
+  console.log(
+    chalk.yellow("OIDC provider for GitHub does not exist. Creating ...")
+  );
+
+  const provider = await iam.send(
+    new CreateOpenIDConnectProviderCommand({
+      Url: `https://token.actions.githubusercontent.com`,
+      ClientIDList: ["sts.amazonaws.com"],
+      ThumbprintList: ["6938fd4d98bab03faadb97b34396831e3780aea1"],
+    })
+  );
+  if (provider.OpenIDConnectProviderArn === undefined) {
+    throw new Error("Failed to create OpenID Connect Provider for GitHub!");
+  }
+
+  return provider.OpenIDConnectProviderArn;
+};
