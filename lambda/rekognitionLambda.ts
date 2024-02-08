b297171b8aefe16a1a8a976b4b06e8b5727fa92e
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2024-02-08 15:11:19 +0100

    feat: cache imagelabels to prevent multiple detectLabels commands on same image

--- lambda/rekognitionLambda.ts
+++ lambda/rekognitionLambda.ts
@@ -1,18 +1,24 @@
 import { RekognitionClient } from '@aws-sdk/client-rekognition'
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import {
+	DynamoDBClient,
+	PutItemCommand,
+	QueryCommand,
+} from '@aws-sdk/client-dynamodb'
 import { fromEnv } from '@nordicsemiconductor/from-env'
 import { S3Client } from '@aws-sdk/client-s3'
 
 import { rekognitionFunction } from './rekognitionLambda/rekognitionFunction.js'
 import { generateThumb } from './rekognitionLambda/generateThumb.js'
 import type { S3Event, S3EventRecord, SQSEvent, SQSRecord } from 'aws-lambda'
+import { replaceSubstringWithColon } from './util/replaceSubstringWithColon.js'
 
 const RekogClient = new RekognitionClient({ region: 'us-east-2' })
 const db = new DynamoDBClient({})
 const s3 = new S3Client({})
 
-const { Table } = fromEnv({
+const { Table, HashTable } = fromEnv({
 	Table: 'TABLE',
+	HashTable: 'HASHTABLE',
 })(process.env)
 
 const rekogFunction = rekognitionFunction(db, RekogClient)
@@ -29,20 +35,58 @@ export const handler = async (event: SQSEvent): Promise<void> => {
 				eventInfo?.Records?.map(async (element: S3EventRecord) => {
 					const bucketName = element.s3.bucket.name
 					const bucketKey = element.s3.object.key
+					const eTag = element.s3.object.eTag
+
 					const maybeThumbnail = await generateThumbnail(bucketName, bucketKey)
 					if ('error' in maybeThumbnail) {
 						console.error('Error generateThumb():', maybeThumbnail.error)
 					}
-					const maybeRekognition = await rekogFunction(
-						bucketName,
-						bucketKey,
-						Table,
+
+					//Check if image has been through rekognition earlier
+					const maybeLabels = await db.send(
+						new QueryCommand({
+							TableName: HashTable,
+							KeyConditionExpression: 'hashTable = :key',
+							ExpressionAttributeValues: {
+								':key': { S: eTag },
+							},
+						}),
 					)
-					if ('error' in maybeRekognition) {
-						console.error(
-							'Error rekognitionFunction(): ',
-							maybeRekognition.error,
+					//If no labels found, run image through rekognition
+					if (maybeLabels === undefined || maybeLabels.Count === 0) {
+						const maybeRekognition = await rekogFunction(
+							bucketName,
+							bucketKey,
+							Table,
+							HashTable,
+							eTag,
 						)
+						if ('error' in maybeRekognition) {
+							console.error(
+								'Error rekognitionFunction(): ',
+								maybeRekognition.error,
+							)
+						}
+					} else {
+						//Labels are in hash table
+
+						const safeKey = replaceSubstringWithColon(bucketKey)
+						//upload Labels to dynamodb
+						const uploadLabels = await db.send(
+							new PutItemCommand({
+								TableName: Table,
+								Item: {
+									...maybeLabels.Items?.[0],
+									image: { S: safeKey },
+								},
+							}),
+						)
+						if (uploadLabels === undefined) {
+							console.error(
+								'error:',
+								new Error(`Failed to upload labels to table: ${Table}`),
+							)
+						}
 					}
 				}) ?? [],
 			)
